9c779bd7f9abb7149f95c53a0f41cc6c06018fbf
jossuema <mmalla1@outlook.com>
2024-06-10 00:00:36 -0500

    Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://reinte.github.io/landing-page',
   integrations: [mdx(), sitemap(), tailwind()]
 });
